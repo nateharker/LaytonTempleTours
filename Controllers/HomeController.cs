59a66a9ac981518f8ab41acc2bfe3622ba9eb09c
bobo0122 <35634479+bobo0122@users.noreply.github.com>
2021-03-16 11:36:00 -0600

    yay

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,7 @@ namespace LaytonTempleTours.Controllers
             return View();
         }
 
+        
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
