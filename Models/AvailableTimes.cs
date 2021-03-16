9c7cc4898d5181269c9f85975d4633b36ba8196a
mbhayden14 <71099393+mbhayden14@users.noreply.github.com>
2021-03-16 12:24:43 -0600

    Add Basic Models and Connection Strings

---
+++ Models/AvailableTimes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaytonTempleTours.Models
+{
+    public class AvailableTimes
+    {
+        DateTime AppointementTime { get; }
+        bool SlotBooked { get; set; } = false;
+    }
+}
---
+++ Models/GroupInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaytonTempleTours.Models
+{
+    public class GroupInfo
+    {
+        [Required]
+        string Name { get; set; }
+        [Required]
+        int Size { get; set; }
+        [Required]
+        [EmailAddress]
+        string Email { get; set; }
+        [Phone]
+        string PhoneNumber { get; set; }
+    }
+}
