5542d598747088548be171dc3eece8dc925b75d3
mbhayden14 <71099393+mbhayden14@users.noreply.github.com>
2021-03-17 18:51:10 -0600

    Add View Model

---
+++ Models/ViewModels/TourViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaytonTempleTours.Models.ViewModels
+{
+    public class TourViewModel
+    {
+        public IEnumerable<AvailableTime> AvailableTimes { get; set; }
+        public IEnumerable<GroupInfo> GroupInfos { get; set; }
+
+    }
+}
